8937056e7bb75995c9ed6bc104da7c3de5a2a229
Alvin Yanson <yansonalvin29@gmail.com>
2021-11-21 16:52:34 +0800

    deployed to binance smart chain
    modified in configs

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+const privateKey = "ea60bc8375b2c51cf675386b754ed4c9ad44f52bb58296354c9baecfb423eeda";
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,10 +10,17 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*", // Match any network id
       gas: 5000000
-    }
+    },
+    testnet: {
+      provider: new HDWalletProvider(privateKey, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   compilers: {
     solc: {
